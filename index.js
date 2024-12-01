5db9b233987b0a2ce03adc2c2daf795a6bcb79cb
phyo wunna kyaw <phyowunnakyaw276@gmail.com>
2024-12-01 09:28:49 -0800

    socket setup for client and server side

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let express = require("express");
+let socket = require("socket.io");
 
 // app_setup
 let app = express();
@@ -11,4 +12,10 @@ let server = app.listen(4000, () => {
 // route_setup
 app.get("/", (res, req) => {
   req.sendFile(__dirname + "/public/index.html");
+});
+
+// socket_setup
+let io = socket(server);
+io.on("connection", (socket) => {
+  console.log("socket connection connected " + socket.id);
 });
